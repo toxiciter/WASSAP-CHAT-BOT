44458fc7b9d7bdf9b7385e6d1a017afb888bd07a
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-08-05 23:08:18 +0600

    Update index.js

--- WCA/index.js
+++ WCA/index.js
@@ -1,1 +1,29 @@
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('[ CLIENT ]: Client is ready!');
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.initialize();
+
+
+async function event() {
+  client.on('message_create', message => {
+	console.log(message);
+    return message;
+});
+};
+
+async function sendMessage(body, chatID, replyMessage, attachment) {
+  client.sendMessage(chatID, body);
+  if (replyMessage) {
+    replyMessage(body);
+  }
+};
