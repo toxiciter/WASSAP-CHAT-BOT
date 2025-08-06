3704b993b95718a25415ff95e6f85c5a4ec799a9
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-08-06 23:37:21 +0600

    Update and rename example.js to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, Location, Poll, List, Buttons, LocalAuth } = require('./index');
+const { Client, Location, Poll, List, Buttons, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -15,7 +15,10 @@ const client = new Client({
      */
     // browserName: 'Firefox',
     puppeteer: { 
-        // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+            ],
         headless: false,
     }
 });
