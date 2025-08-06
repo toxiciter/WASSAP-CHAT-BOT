6ae60171e1409a4cc5b33cb3f1674d6616c3eead
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-08-06 14:12:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,8 @@ const client = new Client({
         headless: true,
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--disable-gpu'
-        ]
+            '--disable-setuid-sandbox'
+            ]
     }
 });
 
