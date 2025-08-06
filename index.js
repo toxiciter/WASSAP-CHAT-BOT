279e001d61852c3e062aad9891e52a61390491b5
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-08-06 13:10:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const { dataType } = require("./utils");
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
