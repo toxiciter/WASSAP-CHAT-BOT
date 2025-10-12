cb23a92b79e336b433540ea8db2f607f02ae05a1
HA SA N <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-10-12 20:43:15 +0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-extensions",
+      "--disable-gpu",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process"
+    ]
   }
 });
 
