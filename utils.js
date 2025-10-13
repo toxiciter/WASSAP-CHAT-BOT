8c5344caa5c2a26c74c43ff4994c881f906ab2e6
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-13 23:17:17 +0600

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { URL } = require("url");
+const path = require("path");
 
 function dataType(input) {
   try {
@@ -12,8 +13,21 @@ function dataType(input) {
   }
 
   return "unknown";
+};
+
+async function getMediaUrl(event) {
+  try {
+  const media = await event.downloadMedia();
+  const fileName = "file_" + Date.now() + ".jpg";
+  const imagePath = path.join(__dirname, "public", fileName);
+  fs.writeFileSync(imagePath, media.data, { encoding: "base64" });
+  return "https://wassap-chat-bot.onrender.com/" + fileName;
+  } catch (e) {
+    throw new Error(e.message)
+  }
 }
 
 module.exports = {
-  dataType
+  dataType,
+  getMediaUrl
 };
