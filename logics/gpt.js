83ce45810f2c2c89c88f48ed58efb824f83e7549
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-14 17:49:08 +0600

    Update gpt.js

--- logics/gpt.js
+++ logics/gpt.js
@@ -1,1 +1,30 @@
+const axios = require("axios");
 
+module.exports = {
+  config: {
+    name: "gpt",
+    author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
+    description: "For conversation with real chatGPT ",
+    guide: "/gpt [YOUR MESSAGE]\n/gpt [YOUR MESSAGE] (with an image)\nexample: /gpt hey ki koro"
+  },
+  logic: async ({ api, args, event, cmdName }) => {
+    const prompt = args.join(" ") || "hie";
+    const url = event.hasMedia ? await api.getMediaUrl(event) : "";
+    const { data } = await axios.get(`https://www.noobx.ct.ws/api/gpt-pro?uid=${event.from}&text=${encodeURIComponent(prompt)}&imageUrl=${url}`);
+    const msg = await api.sendMessage(data.response, event.from, event.id._serialized);
+    global.onReply.set(msg.id._serialized, {
+      cmdName,
+      author: event.from
+    });
+  },
+  reply: async ({ api, event, cmdName }) => {
+    const prompt = event.body;
+    const url = event.hasMedia ? await api.getMediaUrl : "";
+    const { data } = await axios.get(`https://www.noobx.ct.ws/api/gpt-pro?uid=${event.from}&text=${encodeURIComponent(prompt)}&imageUrl=${url}`);
+    const msg = await api.sendMessage(data.response, event.from, event.id._serialized);
+    global.onReply.set(msg.id._serialized, {
+      cmdName,
+      author: event.from
+    });
+  }
+};
