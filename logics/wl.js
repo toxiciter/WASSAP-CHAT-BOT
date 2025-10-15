c06d240ddf776f4239634829357a081f2f2a9c52
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-15 15:30:15 +0600

    Update wl.js

--- logics/wl.js
+++ logics/wl.js
@@ -4,25 +4,30 @@ module.exports = {
     author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
     description: "grant permission to use bot",
     category: "owner",
-    guide: "Usage: /wl [add | -a] <uid> or reply a message\n/wl [remove | -r] <uid>"
+    guide: "Usage: /wl [add | -a] <uid> or reply a message\n/wl [remove | -r] <uid>\n/wl [list | -l]"
   },
-  logic: async ({ wlAdd, wlRemove, event, api, args }) => {
+  logic: async ({ wl, event, api, args }) => {
     const uid = event.message_reply ? event.messageReply.from : args[1];
     switch (args[0]) {
       case "add":
       case "-a": {
-        await wlAdd(uid);
+        await wl.add(uid);
         return event.reply(`✅ ${uid} added to whitelist`);
       }
 
       case "remove":
       case "-r": {
-        await wlRemove(uid);
+        await wl.remove(uid);
         return event.reply(`❌ ${uid} removed from whitelist`);
       }
+      case "list":
+      case "-l": {
+        return await wl.list();
+      }
+        
 
       default:
-        return event.reply("Usage: /wl [add | -a] <uid> or reply a message\n/wl [remove | -r] <uid>");
+        return event.reply("Usage: /wl [add | -a] <uid> or reply a message\n/wl [remove | -r] <uid>\n/wl [list | -l]");
     }
   }
 };
