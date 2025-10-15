bcf433b037054e4f14934f33945b3718d8154d43
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-15 15:23:57 +0600

    Update onEvent.js

--- onEvent.js
+++ onEvent.js
@@ -2,27 +2,34 @@ const fs = require("fs");
 const path = require("path");
 const msg = require("./sendMessage.js");
 const { getMediaUrl } = require("./utils.js");
+const whiteList = require(path.join(__dirname, "API", "models", "WhiteListed.js"));
 
-const wl = require(path.join(__dirname, "API", "models", "WhiteListed.js"));
+const wl = {
+  async add(uid) {  
+    const data = await whiteList.findOne() || await whiteList.create({});
+    if (!data.whitelisted.includes(uid)) {
+      data.whitelisted.push(uid);
+      await data.save();
+      console.log(`✅ Added: ${uid}`);
+    } else {
+      console.log("Already in whitelist!");
+    }
+  },
 
-async function wlAdd(uid) {
-  const data = await wl.findOne() || await wl.create({});
-  if (!data.whitelisted.includes(uid)) {
-    data.whitelisted.push(uid);
-    await data.save();
-    console.log(`✅ Added: ${uid}`);
-  } else {
-    console.log("Already in whitelist!");
+  async remove(uid) {
+    const data = await whiteList.findOne();
+    if (!data) return console.log("No whitelist found!");
+      data.whitelisted = data.whitelisted.filter(x => x !== uid);
+      await data.save();
+      console.log(`❌ Removed: ${uid}`); 
+  },
+  async list() {
+    const { whitelisted } = await whiteList.findOne();
+    return whitelisted;
   }
-}
+};
 
-async function wlRemove(uid) {
-  const data = await wl.findOne();
-  if (!data) return console.log("No whitelist found!");
-  data.whitelisted = data.whitelisted.filter(x => x !== uid);
-  await data.save();
-  console.log(`❌ Removed: ${uid}`);               
-}
+
 
 
 const {
@@ -33,13 +40,18 @@ const {
   font, quiz, album, permission, xnxx, yt, tiktokVideo, grok
       } = require(path.join(__dirname, "API", "hasan.js"));
 
+
+
+
 global.onReply = new Map();
 
 module.exports = async (event, client) => {
   const sendMessage = msg(event, client);
   const prefix = "/";
-  const { whitelisted } = await wl.findOne();
+  const { whitelisted } = await whiteList.findOne();
   const commands = new Map();
+
+  
   const api = {
     sendMessage, getMediaUrl, smsboomber, edit, editpro, upscale_2, imgur,
     dalle_3, imagine, imagine_2, art, img2img,
@@ -47,27 +59,33 @@ module.exports = async (event, client) => {
     prompt, prompt_2, gpt, flux, changebg, flag,
     font, quiz, album, permission, xnxx, yt, tiktokVideo, grok 
   };
+  
 
   // [ LOAD COMMAND ]
-  const cmdsPath = path.join(__dirname, "logics");
-  fs.readdirSync(cmdsPath).forEach(file => {
-    if (file.endsWith(".js")) {
-      const cmd = require(path.join(cmdsPath, file));
-      if (cmd?.config?.name && typeof cmd.logic === "function") {
-        commands.set(cmd.config.name.toLowerCase(), cmd);
-        console.log("[ COMMAND LOADED ]:", cmd.config.name);
-      }
-    }
-  });
+  try {  
+    const cmdsPath = path.join(__dirname, "logics");  
+    fs.readdirSync(cmdsPath).forEach(file => {    
+      if (file.endsWith(".js")) {      
+        const cmd = require(path.join(cmdsPath, file));      
+        if (cmd?.config?.name && typeof cmd.logic === "function") {        
+          commands.set(cmd.config.name.toLowerCase(), cmd);    
+          console.log("[ COMMAND LOADED ]:", cmd.config.name);      
+        }    
+      }  
+    });
+  } catch (e) {
+    throw new Error("[ LOAD_COMMAND_ERROR ]:", e);
+  }
 
-  try {
 
     //[ CHECK PERMISSION ]
     if (!whitelisted.includes(event.from)) return;
     const { body, senderID, messageID } = event;
     if(!body) return;
 
+  
     // [ LOGIC ]
+  try {
     if (body.startsWith(prefix)) {
       const withoutPrefix = body.slice(prefix.length).trim();
       const split = withoutPrefix.split(/\s+/);
@@ -77,17 +95,21 @@ module.exports = async (event, client) => {
 
       if (!cmd) {
         return sendMessage(
-          "<3 I don't have the command: " + cmdName,
+          "ಠ⁠ᴥ⁠ಠ I don't have the command: " + cmdName,
           senderID,
           messageID
         );
       }
-      await cmd.logic({ api, event, args, cmdName, wlAdd, wlRemove });
+      await cmd.logic({ api, event, args, cmdName, wl });
       return;
     }
+  } catch (e) {
+    throw new Error("[ LOGIC_ERROR ]:", e);
+  }
     
 
     //[ REPLY ]
+  try {
     if (event.hasQuotedMsg) {
       const quoted = await event.getQuotedMessage();
       const Reply = global.onReply.get(quoted.id._serialized);
@@ -95,11 +117,10 @@ module.exports = async (event, client) => {
         const cmd = commands.get(Reply.cmdName);
         if (cmd && typeof cmd.reply === "function") {
           return cmd.reply({ Reply, api, event, cmdName });
-    }
-  }
-}
-    
+        }
+      }
+    } 
   } catch (e) {
-    throw new Error(e.message);
+    throw new Error("[ REPLY_ERROR ]:", e);
   }
 };
