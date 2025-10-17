98a6cba332a43be836793028a4cb0c4a2e4aaee6
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-17 16:10:17 +0600

    Refactor logic function and simplify arguments
    Refactor logic function parameters and remove redundant cases.

--- logics/wl.js
+++ logics/wl.js
@@ -6,19 +6,9 @@ module.exports = {
     category: "owner",
     guide: "{pn} [add | -a] <uid> or reply to a message of that user\n{pn} [remove | -r] <uid> or or reply to a message of that user\n{pn} [list | -l]"
   },
-  logic: async ({ wl, event, info, json, args }) => {
+  logic: async ({ wl, event, args }) => {
     const uid = event.message_reply ? event.messageReply.from : args[1];
-    switch (args[0]) {
-      case "on": {
-        await json.edit("whiteListMode", true);
-        event.reply("✅ Turn on only whitelisted user's can use bot..!")
-      }
-
-      case "off": {
-        await json.edit("whiteListMode", false);
-        event.reply("⚠️ Turn off only whitelisted user's can use bot..!");
-      }
-        
+    switch (args[0]) {  
       case "add":
       case "-a": {
         await wl.add(uid);
