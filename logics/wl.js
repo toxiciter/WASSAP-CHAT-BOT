8da4effc713d4608ade3b3bcee4ed9caa7d83737
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-17 15:29:45 +0600

    Refactor logic function parameters and add cases

--- logics/wl.js
+++ logics/wl.js
@@ -6,9 +6,19 @@ module.exports = {
     category: "owner",
     guide: "{pn} [add | -a] <uid> or reply to a message of that user\n{pn} [remove | -r] <uid> or or reply to a message of that user\n{pn} [list | -l]"
   },
-  logic: async ({ wl, event, api, args }) => {
+  logic: async ({ wl, event, info, json, args }) => {
     const uid = event.message_reply ? event.messageReply.from : args[1];
     switch (args[0]) {
+      case "on": {
+        await json.edit("whiteListMode", true);
+        event.reply("✅ Turn on only whitelisted user's can use bot..!")
+      }
+
+      case "off": {
+        await json.edit("whiteListMode", false);
+        event.reply("⚠️ Turn off only whitelisted user's can use bot..!");
+      }
+        
       case "add":
       case "-a": {
         await wl.add(uid);
