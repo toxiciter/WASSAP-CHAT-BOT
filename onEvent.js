ee15b944222a2e7712d7098f7cfcb605a6363991
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-18 16:50:01 +0600

    Update onEvent.js

--- onEvent.js
+++ onEvent.js
@@ -93,7 +93,7 @@ module.exports = async (event, client) => {
 
       if (!cmd) {
         return sendMessage(
-          `ಠ⁠ᴥ⁠ಠ Command "${cmdName}" does not exist..!`,
+          `༎ຶ⁠‿⁠༎ຶ Command '${cmdName}' does not exist.! Type '${prefix}help' to see all available command..!`,
           chatID,
           messageID
         );
