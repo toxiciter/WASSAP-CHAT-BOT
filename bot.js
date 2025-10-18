72fc57bdcf5c58cdeb57548278812b906b1054d3
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-19 01:19:31 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-//let clientInitialized = false;
 
 mongoose.connect("mongodb+srv://toxiciter:Hasan5&7@toxiciter.9tkfu.mongodb.net/WP-BOT-SESSION?retryWrites=true&w=majority&appName=Toxiciter", {
 		useNewUrlParser: true,
@@ -18,93 +17,87 @@ mongoose.connect("mongodb+srv://toxiciter:Hasan5&7@toxiciter.9tkfu.mongodb.net/W
 }).then(() => {
     console.log("[ MONGODB ]:", "connected");
 });
-   /* if (clientInitialized) {
-      console.log("[ CLIENT ]: Already initialized, skipping...");
-      return;    
-    }*/
-    const store = new MongoStore({ mongoose: mongoose });
-    const client = new Client({
-        authStrategy: new RemoteAuth({
-            store: store,
-            backupSyncIntervalMs: 180000       
-        }),
-        puppeteer: {
-            headless: true,
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',  
-                '--disable-gpu',
-                '--disable-extensions',
-                '--no-first-run',
-                '--no-zygote'
-            ]
-        }
-    });
-
-    //clientInitialized = true;
-    client.initialize();
+       
+const store = new MongoStore({ mongoose: mongoose });   
+const client = new Client({        
+	authStrategy: new RemoteAuth({            
+		store: store,           
+		backupSyncIntervalMs: 180000        
+	}),        
+	puppeteer: {          
+		headless: true,         
+		args: [       
+			'--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',  
+            '--disable-gpu',
+            '--disable-extensions',
+            '--no-first-run',
+            '--no-zygote'   
+		]
+    }
+});
     
-    client.on('qr', async (qr) => {  
-        console.log('[ QR RECEIVED ]:', qr);
-        try {    
-            const { data } = await axios.get(`https://quickchart.io/qr?text=${encodeURIComponent(qr)}`, { responseType: "stream" });    
-            const qrPath = path.join(__dirname, "public", "qr.png");
-            const writer = fs.createWriteStream(qrPath);    
-            data.pipe(writer);
+client.initialize();    
+
+client.on('qr', async (qr) => {  
+    console.log('[ QR RECEIVED ]:', qr);
+    try {    
+        const { data } = await axios.get(`https://quickchart.io/qr?text=${encodeURIComponent(qr)}`, { responseType: "stream" });    
+        const qrPath = path.join(__dirname, "public", "qr.png");
+        const writer = fs.createWriteStream(qrPath);    
+        data.pipe(writer);
     
-            writer.on('finish', async () => {      
-                console.log('QR saved at:', qrPath);    
-            });  
-        } catch (error) {    
-            console.error('QR generate error:', error);     
-        }   
+        writer.on('finish', async () => {      
+            console.log('QR saved at:', qrPath);    
+        });  
+    } catch (error) {    
+        console.error('QR generate error:', error);     
+    }   
+});
+
+client.on('ready', () => {
+    console.log('✅ Client is ready!');
+});
+
+client.on('authenticated', () => {
+    console.log('✅ AUTHENTICATED');
+});
+
+client.on('auth_failure', (msg) => {
+    console.error('❌ AUTH FAILURE:', msg);
+});
+
+
+client.on('loading_screen', (percent, message) => {  
+    console.log("[ LOADING ]:", percent + "%", message);
+});
+
+
+client.on('remote_session_saved', () => {
+    console.log("[ SESSION ]:", "Successfully saved");
+});
+
+
+client.on('message_create', async (event) => {    
+    const custom = Object.assign(event, {	
+		senderID: event.from,
+        chatID: await event._getChatId(),
+        messageID: event.id._serialized,
+        message_reply: event.hasQuotedMsg,
+        messageReply: await event.getQuotedMessage()    
     });
-
-    client.on('ready', () => {
-        console.log('✅ Client is ready!');
-    });
-
-    client.on('authenticated', () => {
-        console.log('✅ AUTHENTICATED');
-    });
-
-    client.on('auth_failure', (msg) => {
-        console.error('❌ AUTH FAILURE:', msg);
-    });
-
-
-    client.on('loading_screen', (percent, message) => {  
-        console.log("[ LOADING ]:", percent + "%", message);
-    });
-
-
-    client.on('remote_session_saved', () => {
-        console.log("[ SESSION ]:", "Successfully saved");
-    });
-
-
-    client.on('message_create', async (event) => {
-    
-        const custom = Object.assign(event, {
-			senderID: event.from,
-            chatID: await event._getChatId(),
-            messageID: event.id._serialized,
-            message_reply: event.hasQuotedMsg,
-            messageReply: await event.getQuotedMessage()    
-        });
         
-        onEvent(custom, client);
+    onEvent(custom, client);
     
-        console.log("[ EVENT ]:", {       
-            body: event.body,
-            senderID: event.id.remote,
-            messageID: event.id._serialized,
-            isMedia: event.hasMedia,
-            message_reply: event.hasQuotedMsg    
-        });
+    console.log("[ EVENT ]:", {       
+        body: event.body,
+        senderID: event.id.remote,
+        messageID: event.id._serialized,
+        isMedia: event.hasMedia,
+        message_reply: event.hasQuotedMsg    
     });
-//});
+});
 
 
 app.get('/qr', (req, res) => {
