4d6b205dbecddad5230bbfb64eee31417de3142d
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-24 10:41:49 +0600

    Delete API/models/WhiteListed.js

--- API/models/WhiteListed.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const whitelistSchema = new mongoose.Schema({
-  whitelisted: { type: [String], default: ["8801843152929@c.us"] }
-});
-
-module.exports = mongoose.model("Whitelist", whitelistSchema);
