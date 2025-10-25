6b04ba1a56f29be2eb1413f827f8d02688241205
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-25 12:31:31 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -142,7 +142,7 @@ app.get('/qr', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`running on: http://localhost:${PORT}`);
 });
