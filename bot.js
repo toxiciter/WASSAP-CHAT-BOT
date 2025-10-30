746a7adfe9a07ce3a98e2ab8c566c7473a13fca6
HA SA N <146573168+toxiciter69@users.noreply.github.com>
2025-10-30 14:29:13 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 	const client = new Client({
 		authStrategy: new RemoteAuth({	
 			store: store,	
-			backupSyncIntervalMs: 180000	
+			backupSyncIntervalMs: 600000	
 		}),
 		puppeteer: {		
 			headless: true,		
